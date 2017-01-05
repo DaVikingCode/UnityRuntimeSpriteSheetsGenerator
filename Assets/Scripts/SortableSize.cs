73463275451886e9daf426b42a7dfa7fc5ccffe9
Aymeric <aymeric.lamboley@gmail.com>
2017-01-05 14:40:34 +0100

    Started Unity port

---
+++ Assets/Scripts/SortableSize.cs
@@ -0,0 +1,13 @@
+﻿public class SortableSize {
+
+	public int width;
+	public int height;
+	public int id;
+
+	public SortableSize(int width, int height, int id) {
+
+		this.width = width;
+		this.height = height;
+		this.id = id;
+	}
+}
